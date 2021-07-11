06437073391ef80296e98844034c33f70d3b781d
Aaditya Paithane <51539518+aadityamp01@users.noreply.github.com>
2021-07-11 23:42:08 +0530

    activity_workout Updated

---
+++ app/src/main/java/com/example/aaworkout/WorkoutActivity.kt
@@ -0,0 +1,19 @@
+package com.example.aaworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.aaworkout.databinding.ActivityMainBinding
+
+class WorkoutActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+}
