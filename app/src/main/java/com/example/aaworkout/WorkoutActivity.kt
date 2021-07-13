e37d7bba13de6a165273bae85ab79f66e1a5953c
Aaditya Paithane <51539518+aadityamp01@users.noreply.github.com>
2021-07-13 23:54:22 +0530

    Timer functionality added

--- app/src/main/java/com/example/aaworkout/WorkoutActivity.kt
+++ app/src/main/java/com/example/aaworkout/WorkoutActivity.kt
@@ -2,9 +2,25 @@ package com.example.aaworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.Toast
 import com.example.aaworkout.databinding.ActivityWorkoutBinding
 
 class WorkoutActivity : AppCompatActivity() {
+
+    private var restTimer: CountDownTimer? =
+        null // Variable for Rest Timer and later on we will initialize it.
+    private var restProgress =
+        0 // Variable for timer progress. As initial the rest progress is set to 0. As we are about to start.
+
+    private var exerciseTimer: CountDownTimer? = null // Variable for Exercise Timer and later on we will initialize it.
+    private var exerciseProgress = 0 // Variable for exercise timer progress. As initial the exercise progress is set to 0. As we are about to start.
+
+/*    private var exerciseList: ArrayList<ExerciseModel>? = null // We will initialize the list later.
+    private var currentExercisePosition = -1 // Current Position of Exercise.*/
+
+
     private lateinit var binding: ActivityWorkoutBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +37,36 @@ class WorkoutActivity : AppCompatActivity() {
         binding.toolbarWorkoutActivity.setNavigationOnClickListener {
             onBackPressed()
         }
-
-
     }
+
+    /**
+     * Function is used to set the progress of timer using the progress
+     */
+    private fun setRestProgressBar() {
+
+        binding.progressBar.progress = restProgress // Sets the current progress to the specified value.
+
+        /**
+         * @param millisInFuture The number of millis in the future from the call
+         *   to {#start()} until the countdown is done and {#onFinish()}
+         *   is called.
+         * @param countDownInterval The interval along the way to receive
+         *   {#onTick(long)} callbacks.
+         */
+        // Here we have started a timer of 10 seconds so the 10000 is milliseconds is 10 seconds and the countdown interval is 1 second so it 1000.
+        restTimer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++ // It is increased to ascending order
+                binding.progressBar.progress = 10 - restProgress // Indicates progress bar progress
+                binding.tvTimer.text =
+                    (10 - restProgress).toString()  // Current progress is set to text view in terms of seconds.
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@WorkoutActivity, "Here we will add excercise", Toast.LENGTH_SHORT).show()
+            }
+        }.start()
+    }
+
+
 }
