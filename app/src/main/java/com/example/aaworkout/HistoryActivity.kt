75b711d726102a1ca5e65463ca6b8abc730d2e7c
Aaditya Paithane <51539518+aadityamp01@users.noreply.github.com>
2021-07-26 22:57:21 +0530

    Toolbar added HistoryActivity

--- app/src/main/java/com/example/aaworkout/HistoryActivity.kt
+++ app/src/main/java/com/example/aaworkout/HistoryActivity.kt
@@ -2,10 +2,24 @@ package com.example.aaworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.aaworkout.databinding.ActivityHistoryBinding
 
 class HistoryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHistoryBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_history)
+
+        binding = ActivityHistoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbarHistoryActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.toolbarHistoryActivity.title = "BMI CALCULATOR"
+
+        binding.toolbarHistoryActivity.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
